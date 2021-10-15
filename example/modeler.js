2e2dd6313eeaec0f7d3b7904e800c10fa8b2e3ba
Luca Bonora <luca.bonora@sharedchains.com>
2021-10-15 14:37:49 +0200

    Updated dependencies and example library

--- example/modeler.js
+++ example/modeler.js
@@ -3,28 +3,88 @@
 import BpmnModeler from 'bpmn-js/lib/Modeler';
 import DocViewerModule from '../lib';
 
+import fileDrop from 'file-drops';
+
 import exampleXML from './resources/example.bpmn';
 
-var modeler = new BpmnModeler({
+const url = new URL(window.location.href);
+
+const persistent = url.searchParams.has('p');
+const active = url.searchParams.has('e');
+
+const initialDiagram = (() => {
+  try {
+    return persistent && localStorage['diagram-xml'] || exampleXML;
+  } catch (err) {
+    return exampleXML;
+  }
+})();
+
+function hideDropMessage() {
+  const dropMessage = document.querySelector('.drop-message');
+
+  dropMessage.style.display = 'none';
+}
+
+if (persistent) {
+  hideDropMessage();
+}
+
+const ExampleModule = {
+  __init__: [
+    [ 'eventBus', 'bpmnjs', function(eventBus, bpmnjs) {
+
+      if (persistent) {
+        eventBus.on('commandStack.changed', function() {
+          bpmnjs.saveXML().then(result => {
+            localStorage['diagram-xml'] = result.xml;
+          });
+        });
+      }
+
+    } ]
+  ]
+};
+
+const modeler = new BpmnModeler({
   container: '#canvas',
-  additionalModules: [].concat(BpmnModeler.prototype._modules).concat([
-    DocViewerModule
-  ]),
+  additionalModules: [
+    DocViewerModule,
+    ExampleModule
+  ],
   keyboard: {
     bindTo: document
   }
 });
 
-modeler.importXML(exampleXML)
-  .then(({ warnings }) => {
-    if (warnings.length) {
-      console.warn(warnings);
-    }
+modeler.openDiagram = function(diagram) {
+  return this.importXML(diagram)
+    .then(({ warnings }) => {
+      if (warnings.length) {
+        console.warn(warnings);
+      }
 
-    modeler.get('canvas').zoom('fit-viewport');
+      if (persistent) {
+        localStorage['diagram-xml'] = diagram;
+      }
 
-    window.modeler = modeler;
-  })
-  .catch(err => {
-    console.err(err);
-  });
+      this.get('canvas').zoom('fit-viewport');
+    })
+    .catch(err => {
+      console.error(err);
+    });
+};
+
+document.body.addEventListener('dragover', fileDrop('Open BPMN diagram', function(files) {
+
+  // files = [ { name, contents }, ... ]
+
+  if (files.length) {
+    hideDropMessage();
+    modeler.openDiagram(files[0].contents);
+  }
+
+}), false);
+
+
+modeler.openDiagram(initialDiagram);
