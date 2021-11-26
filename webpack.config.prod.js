0c5374415b2c99614170e632d0309a9c0499875a
Vincenzo1409 <vincenzo.armandi@hotmail.it>
2021-11-26 14:24:58 +0100

    Modal Components fixes

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,16 +1,39 @@
-module.exports = {
-  mode: 'production',
-  entry: './lib/docviewer/docViewer.js',
-  externals: {
-    jquery: 'jQuery',
-    bootstrap: 'bootstrap'
-  },
-  output: {
-    library: 'DocViewer',
-    libraryTarget: 'window',
-    libraryExport: 'default',
-    path: __dirname + '/dist/',
-    filename: 'docViewer.bundle.js',
-  },
-  devtool: 'source-map'
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = (env, argv) => {
+  const mode = argv.mode || 'development';
+
+  const config = {
+    mode,
+    entry: {
+      'bpmn-js-documentation-viewer': './lib/docViewer/docViewer.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: __dirname + '/dist',
+      library: 'docViewerModule',
+      libraryTarget: 'window',
+      libraryExport: 'default'
+    },
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: './assets/**/*', to: '.' },
+
+          // { from: './node_modules/bpmn-js/dist/assets/**/*', to: '.' }
+        ]
+      })
+    ],
+    externals: {
+      'jquery': 'jQuery',
+      'bootstrap': 'bootstrap'
+    },
+    devtool: 'cheap-module-source-map'
+  };
+
+  if (mode === 'production') {
+    config.devtool = 'source-map';
+  }
+
+  return config;
 };
